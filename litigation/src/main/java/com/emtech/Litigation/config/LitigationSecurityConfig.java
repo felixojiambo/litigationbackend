528fdafc659b998b8be1e52292d648094a89e9d5
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-22 00:48:04 -0700

    secure litigation service

---
+++ litigation/src/main/java/com/emtech/Litigation/config/LitigationSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.emtech.Litigation.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class LitigationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .authorizeRequests()
+                .antMatchers("/litigation/**").authenticated()
+                .anyRequest().permitAll();
+    }
+}
