4e713f4a604ad2e906c0f021e866464d130f5786
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-29 02:12:49 -0700

    litigation alternative dispute resolution controller

---
+++ litigation/src/main/java/com/emtech/Litigation/controllers/LitigationCaseADRController.java
@@ -0,0 +1,23 @@
+package com.emtech.Litigation.controllers;
+import com.emtech.Litigation.services.LitigationCaseADRService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/litigation")
+public class LitigationCaseADRController {
+
+    private final LitigationCaseADRService litigationCaseADRService;
+
+    @Autowired
+    public LitigationCaseADRController(LitigationCaseADRService litigationCaseADRService) {
+        this.litigationCaseADRService = litigationCaseADRService;
+    }
+
+    @PostMapping("/{caseId}/adr")
+    public ResponseEntity<Void> initiateADR(@PathVariable Long caseId) {
+        litigationCaseADRService.initiateADRForLitigationCase(caseId);
+        return ResponseEntity.ok().build();
+    }
+}
